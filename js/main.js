4b7e25f1e3edfe4b4c6d3f9307ab52b354512c83
MarieShin <hotis050@gmail.com>
2021-11-09 19:01:31 +0900

    11/09 commu - slick 으로 변경,  유튭 추가, products 추가

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 // library Swiper
-var swiper_communi = new Swiper('.communi-slider', {
-      slidesPerView: 4,
-      spaceBetween: 20,
-      centeredSlides: true,
-      grabCursor: true,
+// var swiper_communi = new Swiper('.communi-slider', {
+//       slidesPerView: 4,
+//       spaceBetween: 20,
+//       centeredSlides: true,
+//       grabCursor: true,
+//       navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//       },
+//       // loop: true,
+// });
+
+
+var swiper_communi = new Swiper('.products-slider', {
       navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -11,9 +24,35 @@ var swiper_communi = new Swiper('.communi-slider', {
       pagination: {
             el: '.swiper-pagination',
             clickable: true,
+            dymanmicBullets: true
       },
       // loop: true,
+});
 
+// slick slider
+$('.communi-slider').slick({
+      // centerPadding: '60px',
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 800,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      responsive: [
+            {
+                  breakpoint: 767,
+                  settings: {
+                        dots: false,
+                        centerPadding: '45px',
+                        slidesToShow: 1
+                  }
+            }
+      ]
 });
 
 // navbar sticky
