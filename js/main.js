bce4e15ef6d80c895a41d1ae888bfd1c1af4142d
MarieShin <hotis050@gmail.com>
2021-11-11 18:41:44 +0900

    11/11 하위메뉴 추가, 커뮤니티 slider 수정, 이미지 등록삭제

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
-// library Swiper
-// var swiper_communi = new Swiper('.communi-slider', {
-//       slidesPerView: 4,
-//       spaceBetween: 20,
-//       centeredSlides: true,
-//       grabCursor: true,
-//       navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//       },
-//       pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true,
-//       },
-//       // loop: true,
-// });
-
+// swiper slider (Products)
 var brand_name = ["맥심", "맥스웰하우스", "카누", "맥심티오피", "Frima", "현미녹차", "미떼", "포스트", "오레오", "필라델피아"];
 
 var swiper_communi = new Swiper('.products-slider', {
-      navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-      },
+      // navigation: {
+      //       nextEl: '.swiper-button-next',
+      //       prevEl: '.swiper-button-prev',
+      // },
       pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -40,7 +24,9 @@ var swiper_communi = new Swiper('.products-slider', {
       speed: 1500,
 });
 
-// slick slider
+// slick slider (Community)
+var item_name = ['사', '회', '공', '헌', '·', 'N', 'E', 'W', 'S', '·'];
+
 $('.communi-slider').slick({
       // centerPadding: '60px',
       dots: true,
@@ -54,6 +40,9 @@ $('.communi-slider').slick({
       centerMode: true,
       pauseOnHover: false,
       pauseOnFocus: false,
+      customPaging: function(slick, index) {
+            return '<a>' + (item_name[index]) + '</a>';
+      },
       responsive: [
             {
                   breakpoint: 767,
@@ -66,17 +55,51 @@ $('.communi-slider').slick({
       ]
 });
 
-// navbar sticky
-// window.onscroll = function() { onScroll() };
+$(function() {
+      var gnbFlag = false;
+      var gnbIndex;
 
-// var navbar = document.getElementById(".main_gnb");
-// var sticky = navbar.offsetTop;
+      $('.gnb li').on({
+            "mouseenter" : function() {
+                  /* lnb show */
+                  if (gnbIndex != undefined) {
+                        $(".lnb").eq(gnbIndex).css("display", "none");
+                  }
 
-// function onScroll() {
-//       if (window.pageYOffset >= sticky) {
-//             navbar.classList.add("sticky");
-//       } else {
-//             navbar.classList.remove("sticky");
-      
-//       }
-// }
+                  gnbIndex = $(this).index();
+                  gnbFlag = true;
+
+                  // 애니메이션 효과 적용
+                  $(".lnb_container").removeClass("disappear");
+                  $(".lnb_container").addClass("appear");
+                  $(".lnb_container").show();
+
+                  $(".lnb").css("display", "inline-block"); 
+
+                  /* 액티브 바 show */
+
+                  $(".active_bar").show();
+
+                  // 선택된 li의 left와 width를 구해 active_bar의 위치와 길이를 조절한다.
+                  var listLeft = $(this).offset().left;
+                  var listWidth = $(this).width();
+
+                  $(".active_bar").width(listWidth);
+                  $(".active_bar").offset({left: listLeft});
+            }
+      });
+
+      /* 하위 메뉴 안보이게 처리 */
+      $(".lnb_container").on({
+            "mouseleave" : function() {
+                  if (gnbFlag) {
+                        $(".lnb_container").removeClass("appear");
+                        $(".lnb_container").addClass("disappear");
+                        $(".lnb_container").hide();
+                        $(".active_bar").hide();
+                  } else {
+                        return;
+                  }
+            }      
+      });
+});
