78a2a4e97ebfde4dfb0516e7b7b186d94ca7180d
MarieShin <hotis050@gmail.com>
2021-11-15 00:29:36 +0900

    11/14 commu layout 수정, products 모바일버전 수정, 모바일메뉴 추가

---
+++ js/common.js
@@ -0,0 +1,90 @@
+// 모바일 버전 상단 메뉴버튼
+$(".trigger").on({
+    "click" : function() {
+        if ($(".mobile.hamburger").is(":visible")) {
+            $(".mobile.hamburger").hide();
+            $(".mobile.close").show();
+
+            $("#main_content").hide();
+            $("#main_footer").hide();
+            $("#mobile_menu").empty();
+
+            var nav = $(".main_gnb").clone();
+            $("#mobile_menu").append(nav);
+            $("#mobile_menu").show();
+
+        } else {
+            $(".mobile.hamburger").show();
+            $(".mobile.close").hide();
+
+            $("#main_content").show();
+            $("#main_footer").show();
+            $("#mobile_menu").hide();
+        }
+    }
+});
+
+$(window).resize(function() {
+    var width = $(window).width();
+
+    if (width > 767) {
+          if ($("#mobile_menu").is(":visible")) {
+                $(".mobile.hamburger").show();
+                $(".mobile.close").hide();
+
+                $("#main_content").show();
+                $("#main_footer").show();
+                
+                $("#mobile_menu").hide();
+          }
+    } 
+});
+
+/* 하위 메뉴, active bar */
+
+var gnbFlag = false;
+var gnbIndex;
+
+$('.gnb li').on({
+        "mouseenter" : function() {
+            /* lnb show */
+            if (gnbIndex != undefined) {
+                    $(".lnb").eq(gnbIndex).css("display", "none");
+            }
+
+            gnbIndex = $(this).index();
+            gnbFlag = true;
+
+            // 애니메이션 효과 적용
+            $(".lnb_container").removeClass("disappear");
+            $(".lnb_container").addClass("appear");
+            $(".lnb_container").show();
+
+            $(".lnb").css("display", "block"); 
+
+            /* 액티브 바 show */
+
+            $(".active_bar").show();
+
+            // 선택된 li의 left와 width를 구해 active_bar의 위치와 길이를 조절한다.
+            var listLeft = $(this).offset().left;
+            var listWidth = $(this).width();
+
+            $(".active_bar").width(listWidth);
+            $(".active_bar").offset({left: listLeft});
+        }
+});
+
+/* 하위 메뉴 안보이게 처리 */
+$(".lnb_container").on({
+        "mouseleave" : function() {
+            if (gnbFlag) {
+                    $(".lnb_container").removeClass("appear");
+                    $(".lnb_container").addClass("disappear");
+                    $(".lnb_container").hide();
+                    $(".active_bar").hide();
+            } else {
+                    return;
+            }
+        }      
+});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // swiper slider (Products)
 var brand_name = ["맥심", "맥스웰하우스", "카누", "맥심티오피", "Frima", "현미녹차", "미떼", "포스트", "오레오", "필라델피아"];
 
-var swiper_communi = new Swiper('.products-slider', {
+var swiper_products = new Swiper('.products-slider', {
       // navigation: {
       //       nextEl: '.swiper-button-next',
       //       prevEl: '.swiper-button-prev',
@@ -39,6 +39,8 @@ $('.communi-slider').slick({
       centerMode: true,
       pauseOnHover: false,
       pauseOnFocus: false,
+      nextArrow: $('#aro_next'),
+      prevArrow: $('#aro_prev'),
       // customPaging: function(slick, index) {
       //       return '<a>' + (item_name[index]) + '</a>';
       // },
@@ -52,53 +54,4 @@ $('.communi-slider').slick({
                   }
             }
       ]
-});
-
-$(function() {
-      var gnbFlag = false;
-      var gnbIndex;
-
-      $('.gnb li').on({
-            "mouseenter" : function() {
-                  /* lnb show */
-                  if (gnbIndex != undefined) {
-                        $(".lnb").eq(gnbIndex).css("display", "none");
-                  }
-
-                  gnbIndex = $(this).index();
-                  gnbFlag = true;
-
-                  // 애니메이션 효과 적용
-                  $(".lnb_container").removeClass("disappear");
-                  $(".lnb_container").addClass("appear");
-                  $(".lnb_container").show();
-
-                  $(".lnb").css("display", "inline-block"); 
-
-                  /* 액티브 바 show */
-
-                  $(".active_bar").show();
-
-                  // 선택된 li의 left와 width를 구해 active_bar의 위치와 길이를 조절한다.
-                  var listLeft = $(this).offset().left;
-                  var listWidth = $(this).width();
-
-                  $(".active_bar").width(listWidth);
-                  $(".active_bar").offset({left: listLeft});
-            }
-      });
-
-      /* 하위 메뉴 안보이게 처리 */
-      $(".lnb_container").on({
-            "mouseleave" : function() {
-                  if (gnbFlag) {
-                        $(".lnb_container").removeClass("appear");
-                        $(".lnb_container").addClass("disappear");
-                        $(".lnb_container").hide();
-                        $(".active_bar").hide();
-                  } else {
-                        return;
-                  }
-            }      
-      });
 });
